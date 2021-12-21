085ac251a9625c6dbd93bea5346e7565f7896d58
coder <2098240977@qq.com>
2021-12-21 18:30:10 +0800

    update

--- TrIDCore.Demo/Program.cs
+++ TrIDCore.Demo/Program.cs
@@ -14,6 +14,6 @@ var demoFilePath = Path.GetFullPath("DemoFile");
 var results = TrIDEngine.GetExtensions(demoFilePath);
 
 Console.WriteLine($"file extension name: {results.FirstOrDefault().FileExt}");
-Console.WriteLine($"file type: {results.FirstOrDefault().FileExt}");
+Console.WriteLine($"file type: {results.FirstOrDefault().FileType}");
 
 Console.ReadLine();
